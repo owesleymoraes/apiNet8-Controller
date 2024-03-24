c138ef5d0b211a640d927edd4510e977af8a2069
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2024-03-24 16:58:25 -0300

    feat: initializer in the project

---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+namespace apicatalogo.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string? Name { get; set; }
+    public string? ImageUrl { get; set; }
+
+}
