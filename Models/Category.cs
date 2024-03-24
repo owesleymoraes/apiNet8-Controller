950d3346b8ef2aa3a4b4f16064e3d792b8104994
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2024-03-24 20:10:20 -0300

    feat: running first migrations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,9 +1,16 @@
+using System.Collections.ObjectModel;
+
 namespace apicatalogo.Models;
 
 public class Category
 {
+    public Category()
+    {
+        Products = new Collection<Product>();
+    }
     public int CategoryId { get; set; }
     public string? Name { get; set; }
     public string? ImageUrl { get; set; }
+    public ICollection<Product>? Products { get; set; }
 
 }
