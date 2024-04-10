1d082c7975a9a3b91f5aa3559d65a03f8e0cbb1e
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2024-04-09 21:43:47 -0300

    feat: implemented repository generic

--- obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5968b0011e89f34ace107d352976bda60caa9032")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6488d63f65ec114c33ccb7bc5f0b03f9ad43e20")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
