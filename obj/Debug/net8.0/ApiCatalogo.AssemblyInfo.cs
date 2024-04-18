f5498847a4af2b7eb6f9c811940ed2deaded7163
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2024-04-18 20:43:10 -0300

    feat: install automap

--- obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f790d4d8df18ab282e8ce1f05bdd16cc7c57e2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebc2d656a32fd1cb1a52a4547ece6d3ad5659c6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
