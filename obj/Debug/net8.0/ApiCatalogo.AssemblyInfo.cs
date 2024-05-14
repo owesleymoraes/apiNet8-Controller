7655193b158aa0cec3f673d181a074abf94f3420
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2024-05-13 21:43:27 -0300

    feat: adding config swagger authorizer token

--- obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c97c9844236e3f5ab7336f21b807ffe41dbd885d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edf0ad1aadef167e699b0e94857d86ffc38063d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
